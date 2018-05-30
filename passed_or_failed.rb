750b9824cb4165ae5633e30982c25736eecd0ea9
Piotr Kędziora <piterek007@o2.pl>
2018-05-30 11:47:47 +0200

    zad1
    zad 1 zrobione

--- passed_or_failed.rb
+++ passed_or_failed.rb
@@ -1,3 +1,22 @@
 class PassedOrFailed
+    def initialize(students, val)
+        @students=students
+        @val=val
+    end
 
+    def result
+        p=Hash.new
+        f=Hash.new
+        @students.each do |key, value|
+            if value>=@val.to_i
+                p[key] = value
+            else 
+                f[key] = value
+            end
+        end
+        res=Hash.new
+        res[:passed]=p
+        res[:failed]=f
+        puts result
+    end
 end
